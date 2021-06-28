3e50877969c91773b8b8bc5539c03327c6c0143c
blentell <blentell@aol.com>
2021-06-28 15:19:06 -0400

    Update with functions

--- main.js
+++ main.js
@@ -1,7 +1,44 @@
 // Your code here.
+function getFirstName(person) {
+  return person.firstName;
+}
 
+function getLastName(person) {
+  return person.lastName;
+}
 
+function getFullName(person) {
+  return person.firstName + " " + person.lastName;
+}
 
+function setFirstName(person, secondFirstName) {
+    person.firstName = secondFirstName;
+}
+
+function setAge(person, age) {
+  person.age = age;
+}
+
+function giveBirthday(person) {
+  if (person.age === undefined) {
+    person["age"] = 0;
+  }
+  person.age += 1;
+}
+
+function marry(person1, person2) {
+  person1.married = true;
+  person2.married = true;
+  person2.spouseName = getFullName(person1);
+  person1.spouseName = getFullName(person2);
+}
+
+function divorce(person1, person2) {
+  person1.married = false;
+  person2.married = false;
+  delete person2.spouseName
+  delete person1.spouseName
+}
 
 
 // Our code here. Don't touch!
